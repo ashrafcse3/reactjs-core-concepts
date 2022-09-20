c305af5d99d23cc6fdb8acf3e32c26d55dd79838
Ashraful Alam <ashrafcse3@gmail.com>
2022-09-20 12:34:24 +0100

    jsx, dynamic content, dynamic style in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+const musicNumber = 444;
+const singer = { name: 'Dr. Mahfuz', job: 'singer' };
+const singer2 = { name: 'eva rahman', job: 'designer' };
+
+const singerStyle = {
+  color: 'purple',
+  background: 'yellow'
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>JSX</h1>
+      <h2>Emmet is done</h2>
+      <div className="container">
+        <h3>Hello dude. React. How are you..</h3>
+      </div>
+      {/* <div className="music">
+        <p>Name: {222 + musicNumber}</p>
+      </div> */}
+      <div className="musician" style={singerStyle}>
+        <p>Musician Name: <strong>{singer.name}</strong></p>
+        <p>Musician Job: <strong>{singer.job}</strong></p>
+      </div>
+      <div className="musician" style={{ color: 'hotpink' }}>
+        <p>Musician Name: <strong>{singer2.name}</strong></p>
+        <p>Musicain Job: <strong>{singer2.job}</strong></p>
+      </div>
+    </div >
   );
 }
 
